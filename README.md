e6bcafe48eaadbc1b8e892a0ec241f59fddd0eee
ephe-meral <j.appel.online@gmail.com>
2015-02-08 19:37:42 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+[![Build Status](https://travis-ci.org/entice/logic.svg)](https://travis-ci.org/entice/logic)
+
+Entice.Logic
+========
+
+Provides the gameplay logic, as entity behaviours.
---
+++ mix.exs
@@ -0,0 +1,14 @@
+defmodule Entice.Logic.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :entice_logic,
+     version: "0.0.1",
+     elixir: "~> 1.0",
+     deps: deps]
+  end
+
+  defp deps do
+    [{:entice_entity, github: "entice/entity", ref: "0e6e7848a3fe094193e72de9a06158af6a81dc3d"}]
+  end
+end
