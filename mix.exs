a4c76e730f6d0052fccbc760b0c572754d328bef
ephe-meral <j.appel.online@gmail.com>
2015-04-02 11:52:27 +0200

    bump entity + skill vers

--- mix.exs
+++ mix.exs
@@ -13,8 +13,8 @@ defmodule Entice.Logic.Mixfile do
   end
 
   defp deps do
-    [{:entice_entity, github: "entice/entity", ref: "05846160142df4d8c20b19b5aca55b9ba748d973"},
-     {:entice_skill, github: "entice/skill", ref: "894687cb4e5d08494084621745841243296caaf6"},
+    [{:entice_entity, github: "entice/entity", ref: "494ccc4453cb663a1838c00c991621ba9782f862"},
+     {:entice_skill, github: "entice/skill", ref: "6c6a83936b543c52a6eebb07e1b6b47dc2a5fe1e"},
      {:uuid, "~> 0.1.5"},
      {:inflex, "~> 0.2.5"}]
   end
