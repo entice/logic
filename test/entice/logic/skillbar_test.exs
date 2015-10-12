7191396a81fc811e373123e23306a5c6c243ac5b
ephe-meral <j.appel.online@gmail.com>
2015-10-12 19:39:43 +0200

    cleanup skillbar

--- test/entice/logic/skillbar_test.exs
+++ test/entice/logic/skillbar_test.exs
@@ -1,21 +1,17 @@
 defmodule Entice.Logic.SkillBarTest do
   use ExUnit.Case, async: true
   alias Entice.Entity
-  alias Entice.Entity.Test.Spy
   alias Entice.Logic.Skills
   alias Entice.Logic.SkillBar
-  alias Entice.Logic.Vitals.Energy
 
   setup do
     {:ok, entity_id, _pid} = Entity.start
-    SkillBar.register(entity_id)
-    Spy.register(entity_id, self)
-    Entity.put_attribute(entity_id, %Energy{mana: 50})
     {:ok, [entity_id: entity_id]}
   end
 
 
   test "change skill", %{entity_id: eid} do
+    SkillBar.register(eid)
     empty_skills = %SkillBar{}
 
     assert [0,0,0,0,0,0,0,0] = SkillBar.get_skills(eid)
@@ -29,94 +25,12 @@ defmodule Entice.Logic.SkillBarTest do
   end
 
 
-  test "cast/recharge skill w/ casttime + rechargetime", %{entity_id: eid} do
-    assert [3,0,0,0,0,0,0,0] = SkillBar.change_skill(eid, 0, 3) # switch to signet of capture
+  test "get skill", %{entity_id: eid} do
+    SkillBar.register(eid, [1,2,3,4,0,0,0,313373])
 
-    this = self
-    assert {:ok, :normal, _skill} = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1))
-
-    assert_receive %{sender: ^eid, event: {:skillbar_cast_end, 0, _cast_callback, _recharge_callback}}, 2100
-    assert_receive Skills.SignetOfCapture
-
-    assert_receive %{sender: ^eid, event: {:skillbar_recharge_end, 0, _callback}}, 2100
-    assert_receive Skills.SignetOfCapture
-  end
-
-
-  test "cast/recharge skill w/o casttime w/o rechargetime", %{entity_id: eid} do
-    this = self
-    assert {:ok, :instant, _skill} = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1))
-
-    refute_receive %{sender: ^eid, event: {:skillbar_cast_end, 0, _cast_callback, _recharge_callback}}
-    refute_receive Skills.NoSkill
-
-    refute_receive %{sender: ^eid, event: {:skillbar_recharge_end, 0, _recharge_callback}}
-    refute_receive Skills.NoSkill
-  end
-
-
-  test "cast/recharge skill w casttime w/o rechargetime", %{entity_id: eid} do
-    assert [2,0,0,0,0,0,0,0] = SkillBar.change_skill(eid, 0, 2) # switch to resurrection signet
-
-    this = self
-    assert {:ok, :normal, _skill} = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1))
-
-    assert_receive %{sender: ^eid, event: {:skillbar_cast_end, 0, _cast_callback, _recharge_callback}}, 3100
-    assert_receive Skills.ResurrectionSignet
-
-    refute_receive %{sender: ^eid, event: {:skillbar_recharge_end, 0, _recharge_callback}}
-    refute_receive Skills.ResurrectionSignet
-  end
-
-
-  test "cast/recharge skill w/o casttime w rechargetime", %{entity_id: eid} do
-    assert [11,0,0,0,0,0,0,0] = SkillBar.change_skill(eid, 0, 11) # switch to distortion
-
-    this = self
-    assert {:ok, :instant, _skill} = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1))
-
-    refute_receive %{sender: ^eid, event: {:skillbar_cast_end, 0, _cast_callback, _recharge_callback}}
-    refute_receive Skills.Distortion
-
-    assert_receive %{sender: ^eid, event: {:skillbar_recharge_end, 0, _recharge_callback}}, 8100
-    assert_receive Skills.Distortion
-  end
-
-
-  test "not cast skill when already casting", %{entity_id: eid} do
-    assert [3,0,0,0,0,0,0,0] = SkillBar.change_skill(eid, 0, 3) # switch to signet of capture
-
-    this = self
-    assert {:ok, :normal, _skill}   = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1))
-    assert {:error, :still_casting} = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1)) # with casttime > 0
-    assert {:error, :still_casting} = SkillBar.cast_skill(eid, 1, &(send this, &1), &(send this, &1)) # with casttime = 0
-
-    assert_receive %{sender: ^eid, event: {:skillbar_cast_end, 0, _cast_callback, _recharge_callback}}, 2100
-    assert_receive Skills.SignetOfCapture
-  end
-
-
-  test "not cast same skill when recharging, but others", %{entity_id: eid} do
-    assert [6,0,0,0,0,0,0,0] = SkillBar.change_skill(eid, 0, 6) # switch to mantra of earth
-
-    this = self
-    assert {:ok, :instant, _skill}     = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1))
-    assert {:error, :still_recharging} = SkillBar.cast_skill(eid, 0, &(send this, &1), &(send this, &1))
-    assert {:ok, :instant, _skill}     = SkillBar.cast_skill(eid, 1, &(send this, &1), &(send this, &1))
-  end
-
-  test "cast skill with enough energy", %{entity_id: eid} do
-    assert [6,0,0,0,0,0,0,0] = SkillBar.change_skill(eid, 0, 6) # switch to mantra of earth
-
-    assert {:ok, :instant, _skill} = SkillBar.cast_skill(eid, 0, &(&1), &(&1))
-    calculated_mana = 50 - Skills.MantraOfEarth.energy_cost
-    assert %Energy{mana: ^calculated_mana} = Entity.get_attribute(eid, Energy)
-  end
-
-  test "not enough energy", %{entity_id: eid} do
-    assert [6,0,0,0,0,0,0,0] = SkillBar.change_skill(eid, 0, 6) # switch to mantra of earth
-
-    Entity.put_attribute(eid, %Energy{mana: 0})
-    assert {:error, :not_enough_energy} = SkillBar.cast_skill(eid, 0, &(&1), &(&1))
+    assert {:ok, Skills.HealingSignet} = SkillBar.get_skill(eid, 0)
+    assert {:ok, Skills.Bamph} = SkillBar.get_skill(eid, 3)
+    assert {:ok, Skills.NoSkill} = SkillBar.get_skill(eid, 4)
+    assert {:ok, Skills.NoSkill} = SkillBar.get_skill(eid, 7)
   end
 end
