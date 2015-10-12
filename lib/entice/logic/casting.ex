6a5aafa9a47ef0d92262209dca7eb48a6814e5b5
ephe-meral <j.appel.online@gmail.com>
2015-10-12 22:10:07 +0200

    cleanup casting, remove callbacks in favour of PID message-sends

--- lib/entice/logic/casting.ex
+++ lib/entice/logic/casting.ex
@@ -4,46 +4,64 @@ defmodule Entice.Logic.Casting do
   Does not validate if an entity has the skills unlocked or w/e.
   Keeps a timer for casting, and an association from
   skill -> recharge-timer.
+
+  You can pass in a listener PID or nil, and you will get notified
+  of the following events:
+
+      {:skill_casted,           %{entity_id: entity, skill: skill}}
+      {:skill_recharged,        %{entity_id: entity, skill: skill}}
+      {:after_cast_delay_ended, %{entity_id: entity}}
   """
+  use Pipe
   alias Entice.Logic.Casting
   alias Entice.Logic.Vitals.Energy
   alias Entice.Entity
-  use Pipe
+
+
+  @after_cast_delay 250
 
 
   defstruct(
-    cast_timer: nil,  #Why not call this cast_timer if skill has a cast_time ? or vice versa
+    cast_timer: nil,
     after_cast_timer: nil,
     recharge_timers: %{})
+
 
   def register(entity_id),
   do: Entity.put_behaviour(entity_id, Casting.Behaviour, %Casting{})
 
+
   def unregister(entity_id),
   do: Entity.remove_behaviour(entity_id, Casting)
 
-  @doc "Deals with timing and thus might fail. Should be called by the Skillbar"
-  def cast_skill(entity, skill, cast_callback, recharge_callback) when is_atom(skill),
-  do: Entity.call_behaviour(entity, Casting.Behaviour, {:casting_cast_start, cast_callback, recharge_callback, %{target: nil, skill: skill}})
 
-  def cast_skill(entity, target, skill, cast_callback, recharge_callback) when is_atom(skill),
-  do: Entity.call_behaviour(entity, Casting.Behaviour, {:casting_cast_start, cast_callback, recharge_callback, %{target: target, skill: skill}})
+  @doc "Deals with timing and thus might fail. Should be called by the Skillbar"
+  def cast_skill(entity, skill, target, report_to_pid \\ nil) when is_atom(skill) and is_pid(report_to_pid),
+  do: Entity.call_behaviour(entity, Casting.Behaviour, {:casting_cast_start, report_to_pid, %{target: nil, skill: skill}})
+
+
+  @doc "Is there a better way to export this value out of this module?"
+  def after_cast_delay, do: @after_cast_delay
 
 
   defmodule Behaviour do
     use Entice.Entity.Behaviour
 
+
     def init(entity, %Casting{} = casting),
     do: {:ok, entity |> put_attribute(casting)}
 
-    def handle_call({:casting_cast_start, cast_callback, recharge_callback, %{target: _target, skill: skill}}, %Entity{attributes: %{
-      Casting => %Casting{recharge_timers: _recharge_timers},
-      Energy => %Energy{mana: mana}}} = entity) do
+
+    def handle_call(
+        {:casting_cast_start, report_to_pid, %{target: _target, skill: skill}},
+        %Entity{attributes: %{
+          Casting => %Casting{},
+          Energy => %Energy{mana: mana}}} = entity) do
 
       response = can_cast?(skill, entity)
       entity = case response do
         {:ok, _} ->
-          timer = cast_start(skill.cast_time, skill, cast_callback, recharge_callback)
+          timer = cast_start(skill.cast_time, skill, report_to_pid)
           entity
           |> update_attribute(Casting, fn c -> %Casting{c | cast_timer: timer} end)
           |> reduce_mana(mana - skill.energy_cost)
@@ -53,55 +71,63 @@ defmodule Entice.Logic.Casting do
       {:ok, response, entity}
     end
 
+
     def handle_call(event, entity), do: super(event, entity)
 
+
     @doc "This event triggers when the cast ends, it resets the casting timer, calls the skill's callback, and triggers recharge_end after a while."
-    def handle_event({:casting_cast_end, skill, cast_callback, recharge_callback}, entity) do
-      recharge_timer = recharge_start(skill.recharge_time, skill, recharge_callback)
-      after_cast_timer = after_cast_start(250) #Dunno how to define constants
-      cast_callback.(skill)
+    def handle_event({:casting_cast_end, skill, report_to_pid}, entity) do
+      recharge_timer = recharge_start(skill.recharge_time, skill, report_to_pid)
+      after_cast_timer = after_cast_start(Entice.Logic.Casting.after_cast_delay, report_to_pid)
+      if report_to_pid, do: report_to_pid |> send {:skill_casted, %{entity_id: entity.id, skill: skill}}
       {:ok, entity |> update_attribute(Casting,
         fn c ->
-          %Casting{c | cast_timer: nil,
-          after_cast_timer: after_cast_timer,
-          recharge_timers: c.recharge_timers |> Map.put(skill, recharge_timer)}
+          %Casting{c |
+            cast_timer: nil,
+            after_cast_timer: after_cast_timer,
+            recharge_timers: c.recharge_timers |> Map.put(skill, recharge_timer)}
         end)}
     end
 
+
     @doc "This event triggers when a skill's recharge period ends, it resets the recharge timer for the skill."
-    def handle_event({:casting_recharge_end, skill, recharge_callback}, entity) do
-      recharge_callback.(skill)
+    def handle_event({:casting_recharge_end, skill, report_to_pid}, entity) do
+      if report_to_pid, do: report_to_pid |> send {:skill_recharged, %{entity_id: entity.id, skill: skill}}
       {:ok, entity |> update_attribute(Casting, fn c -> %Casting{c | recharge_timers: c.recharge_timers |> Map.delete(skill)} end)}
     end
 
-    def handle_event({:casting_after_cast_end}, entity) do
+
+    def handle_event({:casting_after_cast_end, report_to_pid}, entity) do
+      if report_to_pid, do: report_to_pid |> send {:after_cast_delay_ended, %{entity_id: entity.id}}
       {:ok, entity |> update_attribute(Casting, fn c -> %Casting{c | after_cast_timer: nil} end)}
     end
+
 
     def terminate(_reason, entity),
     do: {:ok, entity |> remove_attribute(Casting)}
 
-    defp reduce_mana(entity, new_mana) do
-      entity |> update_attribute(Energy, fn e -> %Energy{e | mana: new_mana} end)
-    end
 
-    #EVENT TRIGGERS
+    # Internal
 
-    #Waits for #cast_time milliseconds then triggers the cast_end event.
-    defp cast_start(cast_time, skill, cast_callback, recharge_callback)  do #We take cast_time as arg rather than use skill.cast_time in case we want to modify it before
-      self |> Process.send_after({:casting_cast_end, skill, cast_callback, recharge_callback}, cast_time)
-    end
 
-    #Waits for #recharge_time milliseconds then triggers the recharge_end event.
-    defp recharge_start(recharge_time, skill, recharge_callback) do
-      self |> Process.send_after({:casting_recharge_end, skill, recharge_callback}, recharge_time)
-    end
+    defp reduce_mana(entity, new_mana),
+    do: entity |> update_attribute(Energy, fn e -> %Energy{e | mana: new_mana} end)
 
-    defp after_cast_start(after_cast_time) do
-      self |> Process.send_after({:casting_after_cast_end}, after_cast_time)
-    end
 
-    #CAST CONDITIONS
+    defp cast_start(cast_time, skill, report_to_pid),
+    do: start_timer({:casting_cast_end, skill, report_to_pid}, cast_time)
+
+
+    defp recharge_start(recharge_time, skill, report_to_pid),
+    do: start_timer({:casting_recharge_end, skill, report_to_pid}, recharge_time)
+
+
+    defp after_cast_start(after_cast_time, report_to_pid),
+    do: start_timer({:casting_after_cast_end, report_to_pid}, after_cast_time)
+
+
+    defp start_timer(message, time), do: self |> Process.send_after(message, time)
+
 
     defp can_cast?(skill,  %Entity{attributes: %{
       Casting => %Casting{cast_timer: cast_timer, after_cast_timer: after_cast_timer, recharge_timers: recharge_timers},
@@ -122,14 +148,16 @@ defmodule Entice.Logic.Casting do
       end
     end
 
-    defp enough_energy?({:ok, skill}, dmana) when dmana > 0, do: {:ok, skill}
-    defp enough_energy?({:ok, _skill}, _dmana), do: {:error, :not_enough_energy}
+
+    defp enough_energy?({:ok, skill}, mana) when mana > 0, do: {:ok, skill}
+    defp enough_energy?({:ok, _skill}, _mana), do: {:error, :not_enough_energy}
+
 
     defp not_recharging?({:ok, skill}, nil = _recharge_timer), do: {:ok, skill}
     defp not_recharging?(_skill, _recharge_timer), do: {:error, :still_recharging}
 
+
     defp not_casting?({:ok, skill}, nil = _cast_timer, nil = _after_cast_timer), do: {:ok, skill}
     defp not_casting?(_skill, _cast_timer, _after_cast_timer), do: {:error, :still_casting}
-
   end
 end
--- test/entice/logic/casting_test.exs
+++ test/entice/logic/casting_test.exs
@@ -1,71 +1,90 @@
 defmodule Entice.Logic.CastingTest do
   use ExUnit.Case, async: true
   alias Entice.Entity
+  alias Entice.Entity.Attribute
   alias Entice.Entity.Test.Spy
   alias Entice.Logic.Skills
   alias Entice.Logic.Casting
   alias Entice.Logic.Vitals.Energy
+  @moduletag :casting
 
 
+  # setup all tags programmatically
   setup do
-    {:ok, entity_id, _pid} = Entity.start
+    {:ok, entity_id, _pid} = Entity.start_plain
+    Attribute.register(entity_id)
     Casting.register(entity_id)
     Spy.register(entity_id, self)
     Entity.put_attribute(entity_id, %Energy{mana: 50})
     {:ok, [entity_id: entity_id]}
   end
 
-  @tag id: 0, casting: true
+
   test "won't cast when not enough energy", %{entity_id: eid} do
     Entity.put_attribute(eid, %Energy{mana: 0})
-    assert {:error, :not_enough_energy} = Casting.cast_skill(eid, Skills.MantraOfEarth, &(send self, &1), &(send self, &1))
+    assert {:error, :not_enough_energy} = Casting.cast_skill(eid, Skills.MantraOfEarth, nil, self)
   end
 
-  @tag id: 1, casting: true
+
   test "won't cast recharging skill", %{entity_id: eid} do
     Entity.put_attribute(eid, %Energy{mana: 100})
 
     recharge_timers = Map.put(%{}, Skills.MantraOfEarth, 10)
     Entity.update_attribute(eid, Casting, fn c -> %Casting{c | recharge_timers: recharge_timers} end)
-    assert {:error, :still_recharging} = Casting.cast_skill(eid, Skills.MantraOfEarth, &(send self, &1), &(send self, &1))
+    assert {:error, :still_recharging} = Casting.cast_skill(eid, Skills.MantraOfEarth, nil, self)
     Entity.update_attribute(eid, Casting, fn _c -> %Casting{} end)
   end
 
-  @tag id: 2, casting: true
-  test "won't cast already casting", %{entity_id: eid} do
 
-    #Test with casting timer != nil
+  test "won't cast with casting timer != nil", %{entity_id: eid} do
     Entity.update_attribute(eid, Casting, fn c -> %Casting{c | cast_timer: 10} end)
-    assert {:error, :still_casting} = Casting.cast_skill(eid, Skills.HealingSignet, &(send self, &1), &(send self, &1))
-    Entity.update_attribute(eid, Casting, fn _c -> %Casting{} end)
-
-    #Test with after_cast_timer != nil
-    Entity.update_attribute(eid, Casting, fn c -> %Casting{c | after_cast_timer: 10} end)
-    assert {:error, :still_casting} = Casting.cast_skill(eid, Skills.HealingSignet, &(send self, &1), &(send self, &1))
-    Entity.update_attribute(eid, Casting, fn _c -> %Casting{} end)
-
-    #Test with both != nil
-    Entity.update_attribute(eid, Casting, fn c -> %Casting{c | cast_timer: 10, after_cast_timer: 10} end)
-    assert {:error, :still_casting} = Casting.cast_skill(eid, Skills.HealingSignet, &(send self, &1), &(send self, &1))
-    Entity.update_attribute(eid, Casting, fn _c -> %Casting{} end)
+    assert {:error, :still_casting} = Casting.cast_skill(eid, Skills.HealingSignet, nil, self)
   end
 
-  @tag id: 3, casting: true
-  test "cast skill succesfully", %{entity_id: eid} do
-    this = self
-    assert {:ok, Skills.SignetOfCapture} = Casting.cast_skill(eid, Skills.SignetOfCapture, &(send this, &1), &(send this, &1))
 
-    #Check appropriate events happen at appropriate times
-    assert_receive %{sender: ^eid, event: {:casting_cast_end, Skills.SignetOfCapture, _cast_callback, _recharge_callback}}, 3500
-    assert_receive Skills.SignetOfCapture
+  test "won't cast with after_cast_timer != nil", %{entity_id: eid} do
+    Entity.update_attribute(eid, Casting, fn c -> %Casting{c | after_cast_timer: 10} end)
+    assert {:error, :still_casting} = Casting.cast_skill(eid, Skills.HealingSignet, nil, self)
+  end
+
+
+  test "won't cast with both cast_timer and after_cast_timer != nil", %{entity_id: eid} do
+    Entity.update_attribute(eid, Casting, fn c -> %Casting{c | cast_timer: 10, after_cast_timer: 10} end)
+    assert {:error, :still_casting} = Casting.cast_skill(eid, Skills.HealingSignet, nil, self)
+  end
+
+
+  test "check correct cast time", %{entity_id: eid} do
+    assert {:ok, Skills.SignetOfCapture} = Casting.cast_skill(eid, Skills.SignetOfCapture, nil, self)
+    # the timers are only set after casting is done
+    assert nil == Entity.get_attribute(eid, Casting).recharge_timers[Skills.SignetOfCapture]
+    assert nil == Entity.get_attribute(eid, Casting).after_cast_timer
+
+    assert_receive %{sender: ^eid, event: {:casting_cast_end, Skills.SignetOfCapture, _pid}}, (Skills.SignetOfCapture.cast_time + 100)
+    assert_receive {:skill_casted, %{entity_id: ^eid, skill: Skills.SignetOfCapture}}
     assert nil != Entity.get_attribute(eid, Casting).recharge_timers[Skills.SignetOfCapture]
     assert nil != Entity.get_attribute(eid, Casting).after_cast_timer
+  end
 
-    assert_receive %{sender: ^eid, event: {:casting_after_cast_end}}, 350
-    assert_receive %{sender: ^eid, event: {:casting_recharge_end, Skills.SignetOfCapture, _recharge_callback}}, 2100
-    assert_receive Skills.SignetOfCapture
 
-    #Check Casting returned to initial state
+  test "check correct after_cast time", %{entity_id: eid} do
+    assert {:ok, Skills.SignetOfCapture} = Casting.cast_skill(eid, Skills.SignetOfCapture, nil, self)
+    assert nil == Entity.get_attribute(eid, Casting).after_cast_timer
+
+    assert_receive %{sender: ^eid, event: {:casting_after_cast_end, _pid}}, (
+      Skills.SignetOfCapture.cast_time + Entice.Logic.Casting.after_cast_delay + 100)
+    assert_receive {:after_cast_delay_ended, %{entity_id: ^eid}}
+    assert nil == Entity.get_attribute(eid, Casting).after_cast_timer
+  end
+
+
+  test "check correct recharge time", %{entity_id: eid} do
+    assert {:ok, Skills.SignetOfCapture} = Casting.cast_skill(eid, Skills.SignetOfCapture, nil, self)
+    assert nil == Entity.get_attribute(eid, Casting).recharge_timers[Skills.SignetOfCapture]
+
+    assert_receive %{sender: ^eid, event: {:casting_recharge_end, Skills.SignetOfCapture, _pid}}, (
+      Skills.SignetOfCapture.cast_time + Skills.SignetOfCapture.recharge_time + 100)
+    assert_receive {:skill_recharged, %{entity_id: ^eid, skill: Skills.SignetOfCapture}}
     assert nil == Entity.get_attribute(eid, Casting).recharge_timers[Skills.SignetOfCapture]
     assert nil == Entity.get_attribute(eid, Casting).cast_timer
     assert nil == Entity.get_attribute(eid, Casting).after_cast_timer
